d6e5c888b5984310069fbe093f87cdfa69a66152
Shashwat <shashwat.pulak@gmail.com>
2017-10-02 19:13:46 +0530

    Add Action Creater for Create post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -21,7 +23,10 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        //  console.log(values);
+        this.props.createPost(values, ()=>{
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -67,4 +72,6 @@ function validate(values) {
 export default reduxForm({
     validate: validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+   connect(null, {createPost})(PostsNew)
+);
