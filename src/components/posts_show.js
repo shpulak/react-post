e50dfdcf4a59fd3471373e77f538c3f6557f7b98
Shashwat <shashwat.pulak@gmail.com>
2017-10-02 20:35:32 +0530

    Add PostsShow page and add navigation and server fetch

---
+++ src/components/posts_show.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { fetchPost } from '../actions';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class PostsShow extends Component {
+    componentDidMount() {
+        // if (!this.props.post) { // Add this code if you want to cache the post
+            const { id } = this.props.match.params;
+            this.props.fetchPost(id);
+        // }
+    }
+
+    onDeleteClick(){
+        // Call Action creator for delete action
+    }
+    render() {
+        const { post } = this.props;
+        if (!post) {
+            return (<div>Loading...</div>);
+        }
+        return (
+            <div>
+                <Link to="/" className="btn btn-success-outline">Back</Link>
+                <button
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick.bind(this)}
+                >
+                Delete Post
+                </button>
+                <h3 className='display-3'>{post.title}</h3>
+                <h6>Categories : {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps({ posts }, ownProps) {
+    return {
+        post: posts[ownProps.match.params.id]
+    };
+}
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
